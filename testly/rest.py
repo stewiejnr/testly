424c4bed4b1b7f58b9e1b64fabab59ff4dd70a09
Ricardo Stewart <rstewart@egovja.com>
2021-01-04 16:34:17 -0500

    add template for rest and soap commands

--- testly/rest.py
+++ testly/rest.py
@@ -1,1 +1,7 @@
 import requests
+import __main__
+
+def call_restendpoint():
+    if __main__.debug == 1:
+        print("DEBUG: REST url: ", __main__.host)
+    return __main__.sys.exit(0)
--- testly/soap.py
+++ testly/soap.py
@@ -1,1 +1,7 @@
 import zeep
+import __main__
+
+def call_soapendpoint():
+    if __main__.debug == 1:
+        print("DEBUG: SOAP WSDL url: ", __main__.host)
+    return __main__.sys.exit(0)
