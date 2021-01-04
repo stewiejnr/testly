43301f7cd0eeff4a1ef1b0cddc18ea2c6b83e9a5
rphipps <rhadjniwork@gmail.com>
2021-01-04 16:37:14 -0500

    Refactor files

--- testly/behaviour.py
+++ testly/behaviour.py
@@ -1,6 +1,3 @@
+import behave
+import selenium
 
-# requires selenium to perform web testing
-pip install beahve
-# requires web drivers to be installed on pc and placed in 
-# PATH before selenium can interface with selected browsers
-pip install selenium
--- testly/database.py
+++ testly/database.py
@@ -1,5 +1,2 @@
-
-#library for connecting interacting with mysql databases
-pip install mysql-connector-python
-#library for interacting with postgreSQL databases
-pip install psycopg2
+import mysql-connector-python
+import psycopg2
